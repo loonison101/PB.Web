a3da0d28893a29999feb82af457e7dc275b8e09d
Lane Katris <lanekatris@gmail.com>
2016-03-23 19:49:43 -0400

    Deploying. Updated version that will be shown top left since a code change was made for a bug fix for prod.

--- dist/config/prod/config.js
+++ dist/config/prod/config.js
@@ -5,7 +5,7 @@ window._config = {
     authority: 'http://tokenserver.us-west-2.elasticbeanstalk.com/identity',
     redirect_uri: 'http://paintball.us-west-2.elasticbeanstalk.com/js/views/oidccallback.html',
     callbackAsync: 'http://elasticbeanstalk-us-west-2-235680268517.s3-website-us-west-2.amazonaws.com/',
-    version: '1.4',
+    version: '1.5',
     appName: 'PB',
     auth0Domain: 'loonison.auth0.com',
     auth0ClientId: 'OEHso6aTa51lkal5j3pR65aU4oYXSUrv'
